1cb507c77577a85e8a8084d680dadee3ceda7e89
manoahLinks <manoahluka@gmail.com>
2023-01-04 01:32:44 +0100

    changing local database to cloud atlas

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const express = require('express'),
 
 
 const store = new mongoDBSession({
-    uri: process.env.URI,
+    uri: process.env.MONGODB_URI,
     collection: 'mySession'
 }) 
 
--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => {
             console.log("DB connected")
         })
