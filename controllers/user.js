40c346b34c95e960c4639f0b4b4a7824f4a8e154
manoahLinks <manoahluka@gmail.com>
2023-01-05 22:28:50 +0100

    perfecting backend authentication to static functions signing webtokens and comparing and hashing passwords

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 const User = require('../models/user'),
-      jwt = require('jsonwebtoken'),
-      bcrypt = require('bcryptjs')
+      jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+
+    return jwt.sign({_id}, process.env.SECRET_KEY, {expiresIn: '1d'})
+}    
 
 exports.getAllUsers = async (req, res) => {
     try {
@@ -30,57 +34,40 @@ exports.registerUser = async (req, res) => {
 
     let {email, password} = req.body
 
-    try {
+   try {
+    
+        const user = await User.signup(email, password)
 
-        let registeredUser = await User.findOne({email:email})
+        // create a token
+        const token = createToken(user._id)
 
-        if(registeredUser){
-            res.status(400).json({message: 'user already exist in database'})
-        }
-        if(!registeredUser){
-            let newUser = new User({email: email, password})
+        res.status(200).json({email, token}) 
 
-            // generating a salt password
-            const salt = await bcrypt.genSalt(10)
+   } catch (error) {
+        res.status(400).json({error: error.message})
+   }
 
-            // setting the user password to a hashed password
-            newUser.password = await bcrypt.hash(password, salt)
-            
-            newUser.ipAddress = req.socket.remoteAddress
-            // saving user detials to database
-            newUser.save().then((newUser)=>{
-                // generating a token to handle authorization
-                let token = jwt.sign({newUserId: newUser.id}, process.env.SECRET_KEY)
-                res.status(201).json({email, token})
-            })
-        }
-
-        
-        
-    } catch (error) {
-        res.status(400).json(error)
-    }
 }
 
 
 // login logic for User
 exports.loginUser = async (req, res)=>{
 
-    const {email, password} = req.body    
-    // checking for admin email in database
-    let user = await User.findOne({email: email})
-        if(user){
-            // comparing the inputted password and hashed password using bcrypt
-            const validPassword = await bcrypt.compare(password, user.password)
-            if(validPassword){
-                req.session.isAuth = true
-                res.status(200).json({email, token})
-            }else{
-                res.status(400).json({message: 'password is invalid'})
-            }
-        }else{
-            res.status(400).json({message: 'User acct does not exist'})
-        }
+    const {email, password} = req.body 
+
+    try {
+    
+        const user = await User.login(email, password)
+
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token}) 
+
+    } catch (error) {
+         res.status(400).json({error: error.message})
+    }   
+   
 }
 
 
--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const validator = require('validator')
 
 let userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
@@ -7,6 +9,59 @@ let userSchema = new mongoose.Schema({
     password: {type: String, default: 1234},
     ipAddress: {type: String}
 })
+
+userSchema.statics.signup = async function (email, password) {
+
+    // validation
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+
+    if(!validator.isEmail(email)){
+        throw Error('email is not valid')
+    }
+    
+    if(!validator.isStrongPassword(password)){
+        throw Error ('password not strong enough')
+    }
+
+    const exists = await this.findOne({email})
+
+    if(exists){
+        throw Error('Email already in use')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({email, password: hash})
+
+    return user
+
+}
+
+userSchema.statics.login = async function (email, password) {
+
+     // validation
+     if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({email})
+
+    if(!user){
+        throw Error('Incorrect email')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if(!match){
+        throw Error('Incorrect password')
+    }
+
+    return user
+
+}
 
 let User = mongoose.model('user', userSchema)
 
