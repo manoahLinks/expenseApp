c9650745f863ce95bc0823fafb66371101c5dcf7
manoahLinks <manoahluka@gmail.com>
2023-02-09 00:23:29 +0100

    new build with deployment api routes and db

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -11,7 +11,7 @@ export const useSignup = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch(`http://localhost:5500/api/user/register`, {
+        const response = await fetch(`https://expesetracker.herokuapp.com/api/user/register`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
