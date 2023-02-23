bb4842fcb7790fd17361dde21faa471c5aa56e53
manoahLinks <manoahluka@gmail.com>
2023-02-23 04:32:46 +0100

    adding the useFetch custom hook to handle simple fetching in indiviadual components

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import { useAuthContext } from "./useAuthContext"
+
+const useFetch = (url) => {
+
+    const {user} = useAuthContext()
+    const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(null)
+    const [error, setError] = useState(null)
+
+    useEffect(()=>{
+
+        fetch(url, {
+            headers:{
+                'Authorization': `Bearer ${user.token}`
+            }
+        }).then((res)=>{
+            if(!res.ok){
+                throw Error(`no valid response from this resourse`)
+            }
+            return res.json()
+
+        }).then((data)=>{
+
+            setData(data)
+            setError(null)
+            setIsPending(null)
+
+        }).catch((error)=>{
+            setError(error.message)
+        })
+     
+    }, [url])
+    
+    return {data, isPending, error}
+}
+
+export default useFetch;
--- client/src/pages/rawmaterialpages/RecieveRawmaterial.js
+++ client/src/pages/rawmaterialpages/RecieveRawmaterial.js
@@ -1,4 +1,24 @@
+import React ,{useState, useEffect} from 'react' 
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useDataContext } from "../../hooks/useDataContext";
+import useFetch from '../../hooks/useFetch';
+
 const RecieveRawmaterial = () => {
+
+    const {user} = useAuthContext()
+    const {data, dispatch} = useDataContext()
+    const [material, setMaterial] = useState('')
+    const [quantity, setQuantity] = useState('')
+    const [amount, setAmount] = useState('')
+    const [supplier, setSupplier] = useState('')
+
+    const {data:rawmaterial, isPending, error} = useFetch(`http://localhost:5500/api/rawmaterial`)
+    const {data:suppliers} = useFetch(`http://localhost:5500/api/supplier`)
+
+    const handleSubmit = async () => {
+
+    }
+
     return ( 
         <div className="grid grid-cols-1 md:grid-cols-2">
             <div className="flex">
@@ -10,31 +30,41 @@ const RecieveRawmaterial = () => {
                     <label className="text-xs">Raw material</label>
                     <select
                         className="border-slate-300 text-xs"
+                        value={material}
+                        onChange={(e)=>{setMaterial(e.target.value)}}
                     >
                         <option value="">select rawmaterial</option>
-                        <option value="">sugar</option>
-                        <option value="">sugar</option>
-                        <option value="">sugar</option>
-                        <option value="">sugar</option>
+                        {rawmaterial && rawmaterial.map((material)=>(
+                            <option key={material._id} value={material._id}>{material.name}</option>
+                        ))}
                     </select>
                     <div className="grid grid-cols-2 gap-y-2">
                         <label htmlFor="">Quantity</label>
                         <input 
                             type="number"
                             className="text-xs border-slate-300"
+                            value={quantity}
+                            onChange={(e)=>{setQuantity(e.target.value)}}
                         />
 
                         <label htmlFor="">Amount</label>
                         <input 
                             type="number"
                             className="text-xs border-slate-300"
+                            value={amount}
+                            onChange={(e)=>{setAmount(e.target.value)}}
                         />
 
                         <label htmlFor="">supplier</label>
                         <select
                             className="text-xs border-slate-300"
+                            value={supplier}
+                            onChange={(e)=>{setSupplier(e.target.value)}}
                         >
                             <option value="">select supplier</option>
+                            {suppliers && suppliers.map((supplier)=>(
+                               <option value={supplier._id}>{supplier.name}</option> 
+                            ))}
                         </select>
                     </div>
                     <div className="grid grid-cols-3 gap-x-4 mt-8">
