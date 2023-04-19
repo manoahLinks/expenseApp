def0fb38de1426f0be5ae3d8f893be6071a7eadf
manoahLinks <manoahluka@gmail.com>
2023-04-19 07:04:58 +0100

    adding react-toastify to handle alert messaging in attendance page

--- client/src/pages/AttendacePage.js
+++ client/src/pages/AttendacePage.js
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
-import AlertBox from "../components/AlertBox";
+import {ToastContainer, toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const AttendancePage = ({modalOff}) => {
-
-    const [success, setSuccess] = useState(false)
-    const [error, setError] = useState(false)
-    const [message, setMessage] = useState(null)
 
     const handlePresent = () => {
         if (navigator.geolocation) {
@@ -14,30 +11,48 @@ const AttendancePage = ({modalOff}) => {
       
           function findLocation(position) {
               if(position.coords.latitude === 9.0764785 && position.coords.longitude === 7.398574){
-                  setError(false)
-                  setSuccess(true)
-                  setMessage('Welcome once again, your attendance stats has been successfully recorded')
+                    toast.success(`Welcome once again, your attendance stats has been successfully recorded`, {
+                        position: "bottom-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                    });
               }
               else{
-                setSuccess(false)
-                setError(true)
-                setMessage('Your location doesnt match what you cliam')
+                toast.warn('Your location doesnt match what you cliam', {
+                    position: "bottom-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    });
               }
             }  
     }
 
     const handleAbsent = () => {
-        
-        setError(false)
-        setSuccess(true)
-        setMessage('Dear user we hope you are doing great, hope to see you at work soon')
+        toast.error('Dear user we hope you are doing great, hope to see you at work soon!', {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
     }
 
     return ( 
         <div className="flex flex-col inset-0 fixed bg-primary bg-opacity-10 items-center justify-center gap-y-4">
             <div className="flex flex-col items-center justify-center gap-y-4 bg-white md:p-5 p-3 rounded-lg shadow-lg">
-                {error && <AlertBox message={message}></AlertBox>}
-                {success && <AlertBox message={message}></AlertBox> }
                 <div className="flex">
                     <svg onClick={modalOff} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="cursor-pointer w-5 h-5">
                         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clipRule="evenodd" />
@@ -55,6 +70,7 @@ const AttendancePage = ({modalOff}) => {
                     <button className="text-sm font-semibold bg-amber-200 p-2 rounded-md">sick</button>
                 </div>
             </div>
+            <ToastContainer/>
         </div>
      );
 }
